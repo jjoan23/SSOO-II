72b1412295c8f854852d47a1fbc266f8409fbc63
jjoan23 <joanjimenezrigo@gmail.com>
2025-02-14 11:29:27 +0100

    tres funcions

---
+++ bloques.c
@@ -0,0 +1,47 @@
+#include "bloques.h"
+
+
+static int descriptor = 0;
+
+int bmount(const char *camino){
+
+descriptor = open(camino,O_RDWR | O_CREAT,0666); 
+
+if(descriptor ==-1){
+    perror("");
+    return FALLO;
+}
+
+return descriptor;
+}
+
+int bumount(){
+int i = close(descriptor);
+if(i==-1){
+    perror("");
+    return FALLO;
+}
+return EXITO;
+
+}
+
+
+int bwrite(unsigned int nbloque, const void *buf){
+
+ if (lseek(descriptor, nbloque * BLOCKSIZE, SEEK_SET) == -1) {
+        perror("");
+        return FALLO;
+    }
+ssize_t bytes_escritos = write(descriptor, buf, BLOCKSIZE);
+    if (bytes_escritos == -1) {
+        perror("Error en write");
+        return FALLO;
+    }
+
+    return bytes_escritos;
+
+}
+
+int bread(unsigned int nbloque, void *buf){
+
+}
