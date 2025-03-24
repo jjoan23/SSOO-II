cbac27ff3d3fdb279a623454e976c17f230a2090
marcmd2005 <mmateud8@gmail.com>
2025-03-24 13:45:31 +0100

    .

--- nivel4/ficheros_basico.c
+++ nivel4/ficheros_basico.c
@@ -25,7 +25,7 @@ int initSB(unsigned int nbloques, unsigned int ninodos) {
     SB.posUltimoBloqueDatos = nbloques - 1;
     SB.posInodoRaiz = 0;
     SB.posPrimerInodoLibre = 0;
-    SB.cantBloquesLibres = nbloques - 1;
+    SB.cantBloquesLibres = nbloques;
     SB.cantInodosLibres = ninodos;
     SB.totBloques = nbloques;
     SB.totInodos = ninodos;
@@ -398,10 +398,10 @@ int reservar_inodo(unsigned char tipo, unsigned char permisos) {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int obtener_nRangoBL(struct inodo *inodo, unsigned int nblogico, unsigned int *ptr) {
-    if (nblogico < DIRECTOS) {
+    if (nblogico < DIRECTOS) { 
         *ptr = inodo->punterosDirectos[nblogico];
         return 0;
-    } else if (nblogico < INDIRECTOS0) {
+    } else if (nblogico < INDIRECTOS0) {  
         *ptr = inodo->punterosIndirectos[0];
         return 1;
     } else if (nblogico < INDIRECTOS1) {
@@ -421,21 +421,27 @@ int obtener_nRangoBL(struct inodo *inodo, unsigned int nblogico, unsigned int *p
 int obtener_indice(unsigned int nblogico, int nivel_punteros) {
     if (nblogico < DIRECTOS) {
         return nblogico;
+
     } else if (nblogico < INDIRECTOS0) {
-        return nblogico - DIRECTOS;
+        return (nblogico - DIRECTOS);
+
     } else if (nblogico < INDIRECTOS1) {
         if (nivel_punteros == 2) {
-            return (nblogico - INDIRECTOS0) / NPUNTEROS;
+            return ((nblogico - INDIRECTOS0) / NPUNTEROS);
+
         } else if (nivel_punteros == 1) {
-            return (nblogico - INDIRECTOS0) % NPUNTEROS;
+            return ((nblogico - INDIRECTOS0) % NPUNTEROS);
         }
+
     } else if (nblogico < INDIRECTOS2) {
         if (nivel_punteros == 3) {
-            return (nblogico - INDIRECTOS1) / (NPUNTEROS * NPUNTEROS);
+            return ((nblogico - INDIRECTOS1) / (NPUNTEROS * NPUNTEROS));
+
         } else if (nivel_punteros == 2) {
-            return ((nblogico - INDIRECTOS1) % (NPUNTEROS * NPUNTEROS)) / NPUNTEROS;
+            return (((nblogico - INDIRECTOS1) % (NPUNTEROS * NPUNTEROS)) / NPUNTEROS);
+
         } else if (nivel_punteros == 1) {
-            return ((nblogico - INDIRECTOS1) % (NPUNTEROS * NPUNTEROS)) % NPUNTEROS;
+            return (((nblogico - INDIRECTOS1) % (NPUNTEROS * NPUNTEROS)) % NPUNTEROS);
         }
     }
     return FALLO;
