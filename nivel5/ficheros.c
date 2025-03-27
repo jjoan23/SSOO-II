40cb7e4917ffa560bd7fffc5ecbf90a6af21395e
jjoan23 <joanjimenezrigo@gmail.com>
2025-03-27 20:15:30 +0100

    nivel5

---
+++ nivel5/ficheros.c
@@ -0,0 +1,154 @@
+#include "ficheros.h"
+
+int mi_write_f(unsigned int ninodo, const void *buf_original, unsigned int offset, unsigned int nbytes) {
+    struct inodo inodo;
+    if (leer_inodo(ninodo, &inodo) == FALLO) return FALLO;
+
+    if ((inodo.permisos & 2) != 2) {
+        fprintf(stderr, "No hay permisos de escritura\n");
+        return FALLO;
+    }
+
+    unsigned int primerBL = offset / BLOCKSIZE;
+    unsigned int ultimoBL = (offset + nbytes - 1) / BLOCKSIZE;
+    unsigned int desp1 = offset % BLOCKSIZE;
+    unsigned int desp2 = (offset + nbytes - 1) % BLOCKSIZE;
+
+    unsigned char buf_bloque[BLOCKSIZE];
+    unsigned int nbfisico;
+    unsigned int bytes_escritos = 0;
+
+    if (primerBL == ultimoBL) {
+        nbfisico = traducir_bloque_inodo(ninodo, primerBL, 1);
+        if (nbfisico == FALLO) return FALLO;
+
+        if (bread(nbfisico, buf_bloque) == FALLO) return FALLO;
+        memcpy(buf_bloque + desp1, buf_original, nbytes);
+        if (bwrite(nbfisico, buf_bloque) == FALLO) return FALLO;
+
+        bytes_escritos += nbytes;
+    } else {
+        nbfisico = traducir_bloque_inodo(ninodo, primerBL, 1);
+        if (nbfisico == FALLO) return FALLO;
+
+        if (bread(nbfisico, buf_bloque) == FALLO) return FALLO;
+        memcpy(buf_bloque + desp1, buf_original, BLOCKSIZE - desp1);
+        if (bwrite(nbfisico, buf_bloque) == FALLO) return FALLO;
+
+        bytes_escritos += BLOCKSIZE - desp1;
+
+        for (unsigned int bl = primerBL + 1; bl < ultimoBL; bl++) {
+            nbfisico = traducir_bloque_inodo(ninodo, bl, 1);
+            if (nbfisico == FALLO) return FALLO;
+
+            if (bwrite(nbfisico, buf_original + bytes_escritos) == FALLO) return FALLO;
+            bytes_escritos += BLOCKSIZE;
+        }
+
+        nbfisico = traducir_bloque_inodo(ninodo, ultimoBL, 1);
+        if (nbfisico == FALLO) return FALLO;
+
+        if (bread(nbfisico, buf_bloque) == FALLO) return FALLO;
+        memcpy(buf_bloque, buf_original + bytes_escritos, desp2 + 1);
+        if (bwrite(nbfisico, buf_bloque) == FALLO) return FALLO;
+
+        bytes_escritos += desp2 + 1;
+    }
+
+    if (leer_inodo(ninodo, &inodo) == FALLO) return FALLO;
+
+    if (offset + nbytes > inodo.tamEnBytesLog) inodo.tamEnBytesLog = offset + nbytes;
+    inodo.mtime = time(NULL);
+    inodo.ctime = time(NULL);
+
+    if (escribir_inodo(ninodo, &inodo) == FALLO) return FALLO;
+
+    return bytes_escritos;
+}
+
+int mi_read_f(unsigned int ninodo, void *buf_original, unsigned int offset, unsigned int nbytes) {
+    struct inodo inodo;
+    if (leer_inodo(ninodo, &inodo) == FALLO) return FALLO;
+
+    if ((inodo.permisos & 4) != 4) {
+        fprintf(stderr, "No hay permisos de lectura\n");
+        return FALLO;
+    }
+
+    if (offset >= inodo.tamEnBytesLog) return 0;
+
+    if (offset + nbytes > inodo.tamEnBytesLog) nbytes = inodo.tamEnBytesLog - offset;
+
+    unsigned int primerBL = offset / BLOCKSIZE;
+    unsigned int ultimoBL = (offset + nbytes - 1) / BLOCKSIZE;
+    unsigned int desp1 = offset % BLOCKSIZE;
+    unsigned int desp2 = (offset + nbytes - 1) % BLOCKSIZE;
+
+    unsigned char buf_bloque[BLOCKSIZE];
+    unsigned int nbfisico;
+    unsigned int bytes_leidos = 0;
+
+    if (primerBL == ultimoBL) {
+        nbfisico = traducir_bloque_inodo(ninodo, primerBL, 0);
+        if (nbfisico != FALLO) {
+            if (bread(nbfisico, buf_bloque) == FALLO) return FALLO;
+            memcpy(buf_original, buf_bloque + desp1, nbytes);
+        }
+        bytes_leidos += nbytes;
+    } else {
+        nbfisico = traducir_bloque_inodo(ninodo, primerBL, 0);
+        if (nbfisico != FALLO) {
+            if (bread(nbfisico, buf_bloque) == FALLO) return FALLO;
+            memcpy(buf_original, buf_bloque + desp1, BLOCKSIZE - desp1);
+        }
+        bytes_leidos += BLOCKSIZE - desp1;
+
+        for (unsigned int bl = primerBL + 1; bl < ultimoBL; bl++) {
+            nbfisico = traducir_bloque_inodo(ninodo, bl, 0);
+            if (nbfisico != FALLO) {
+                if (bread(nbfisico, buf_original + bytes_leidos) == FALLO) return FALLO;
+            }
+            bytes_leidos += BLOCKSIZE;
+        }
+
+        nbfisico = traducir_bloque_inodo(ninodo, ultimoBL, 0);
+        if (nbfisico != FALLO) {
+            if (bread(nbfisico, buf_bloque) == FALLO) return FALLO;
+            memcpy(buf_original + bytes_leidos, buf_bloque, desp2 + 1);
+        }
+        bytes_leidos += desp2 + 1;
+    }
+
+    inodo.atime = time(NULL);
+    if (escribir_inodo(ninodo, &inodo) == FALLO) return FALLO;
+
+    return bytes_leidos;
+}
+
+int mi_stat_f(unsigned int ninodo, struct STAT *p_stat) {
+    struct inodo inodo;
+    if (leer_inodo(ninodo, &inodo) == FALLO) return FALLO;
+
+    p_stat->tipo = inodo.tipo;
+    p_stat->permisos = inodo.permisos;
+    p_stat->nlinks = inodo.nlinks;
+    p_stat->tamEnBytesLog = inodo.tamEnBytesLog;
+    p_stat->atime = inodo.atime;
+    p_stat->mtime = inodo.mtime;
+    p_stat->ctime = inodo.ctime;
+    p_stat->numBloquesOcupados = inodo.numBloquesOcupados;
+
+    return EXITO;
+}
+
+int mi_chmod_f(unsigned int ninodo, unsigned char permisos) {
+    struct inodo inodo;
+    if (leer_inodo(ninodo, &inodo) == FALLO) return FALLO;
+
+    inodo.permisos = permisos;
+    inodo.ctime = time(NULL);
+
+    if (escribir_inodo(ninodo, &inodo) == FALLO) return FALLO;
+
+    return EXITO;
+}
--- nivel5/leer_sf.c
+++ nivel5/leer_sf.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include "bloques.h"
 #include "ficheros_basico.h"
+#include "ficheros.h"
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -35,46 +36,45 @@ int main(int argc, char *argv[]) {
     printf("cantInodosLibres = %u\n", SB.cantInodosLibres);
     printf("totBloques = %u\n", SB.totBloques);
     printf("totInodos = %u\n", SB.totInodos);
-    
-    printf("\nINODO 1. TRADUCCION DE LOS BLOQUES LOGICOS 8, 204, 30004, 400004 y 468750\n");
+
+    printf("\nINODO 1. TRADUCCIÓN DE LOS BLOQUES LÓGICOS 8, 204, 30004, 400004 y 468750\n");
     unsigned int bloques[] = {8, 204, 30004, 400004, 468750};
     int bf;
     for (int i = 0; i < 5; i++) {
-        bf = traducir_bloque_inodo(1, bloques[i], 1);
-        printf("[traducir_bloque_inodo() -> inodo.punterosDirectos[%u] = %d (reservado BF %d para BL %u)]\n", bloques[i], bf, bf, bloques[i]);
-        
+        bf = traducir_bloque_inodo(1, bloques[i], 0); // Cambiado reservar = 0 para solo traducir
+        if (bf != FALLO) {
+            printf("[traducir_bloque_inodo() -> BF %d para BL %u]\n", bf, bloques[i]);
+        } else {
+            printf("[traducir_bloque_inodo() -> No asignado para BL %u]\n", bloques[i]);
+        }
     }
 
-    
     struct inodo inodo;
-    leer_inodo(0, &inodo);
+    if (leer_inodo(1, &inodo) == FALLO) {
+        perror("Error leyendo el inodo");
+        bumount();
+        return EXIT_FAILURE;
+    }
+
     struct tm *ts;
-    char atime[80], mtime[80], ctime[80], btime[80];
+    char atime[80], mtime[80], ctime[80];
     ts = localtime(&inodo.atime);
     strftime(atime, sizeof(atime), "%a %Y-%m-%d %H:%M:%S", ts);
     ts = localtime(&inodo.mtime);
     strftime(mtime, sizeof(mtime), "%a %Y-%m-%d %H:%M:%S", ts);
     ts = localtime(&inodo.ctime);
     strftime(ctime, sizeof(ctime), "%a %Y-%m-%d %H:%M:%S", ts);
-    ts = localtime(&inodo.btime);
-    strftime(btime, sizeof(btime), "%a %Y-%m-%d %H:%M:%S", ts);
-    printf("\nDATOS DEL DIRECTORIO RAIZ\n");
+
+    printf("\nDATOS DEL INODO 1\n");
     printf("tipo: %c\n", inodo.tipo);
     printf("permisos: %u\n", inodo.permisos);
-
     printf("atime: %s\n", atime);
     printf("mtime: %s\n", mtime);
     printf("ctime: %s\n", ctime);
-    printf("btime: %s\n", btime);
-    
     printf("nlinks: %u\n", inodo.nlinks);
     printf("tamEnBytesLog: %u\n", inodo.tamEnBytesLog);
     printf("numBloquesOcupados: %u\n", inodo.numBloquesOcupados);
-    
 
-    
-    printf("\nSB.posPrimerInodoLibre = %u\n", SB.posPrimerInodoLibre + 1);
-    
     bumount();
     return EXIT_SUCCESS;
 }
